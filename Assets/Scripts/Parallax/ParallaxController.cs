7bebe591a297142b077fb72d86483f622a354071
RandomZiom <dkonik10@gmail.com>
2023-06-24 14:48:18 +0200

    scrolling background

---
+++ Assets/Scripts/Parallax/ParallaxController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Parallax.Abstraction;
+using UnityEngine;
+
+namespace Parallax
+{
+    public class ParallaxController: MonoBehaviour, IParallaxController
+    {
+        [SerializeField]
+        private List<ParallaxElement> _parallaxElements;
+
+        private void Awake()
+        {
+            ServiceLocator.ServiceLocator.Instance.Register<IParallaxController>(this);
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.ServiceLocator.Instance.Deregister<IParallaxController>();
+        }
+
+        public void SetState(bool isMoving)
+        {
+            _parallaxElements.ForEach(element => 
+                element.IsMoving = isMoving);
+        }
+    }
+}
---
+++ Assets/Scripts/Parallax/ParallaxElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Parallax
+{
+    public class ParallaxElement: MonoBehaviour
+    {
+        [SerializeField]
+        private float _scrollSpeed = 1f;
+        
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        public bool IsMoving { get; set; } = true;
+        
+        private const float START_HEIGHT = 25f;
+        
+        private void FixedUpdate()
+        {
+            if(!IsMoving)
+                return;
+            
+            _spriteRenderer.size = new Vector2( _spriteRenderer.size.x, START_HEIGHT + Time.time * _scrollSpeed);
+        }
+    }
+}
