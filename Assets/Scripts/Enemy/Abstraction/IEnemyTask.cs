697d2cdadcb4017fbd4d1f04d7131514c1719a20
RandomZiom <dkonik10@gmail.com>
2023-06-25 13:08:59 +0200

    added working butterfly

---
+++ Assets/Scripts/Enemy/Abstraction/IEnemyTask.cs
@@ -0,0 +1,9 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Enemy.Abstraction
+{
+    public interface IEnemyTask
+    {
+        public UniTask<EnemyTaskResult> Execute(EnemyBehaviour behaviour);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Enemy.Abstraction;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace Enemy
+{
+    public class EnemyBehaviour: SerializedMonoBehaviour
+    {
+        [OdinSerialize]
+        private List<IEnemyTask> _tasks;
+        
+        private UniTask<EnemyTaskResult> _currentTask;
+        private EnemyTaskResult _lastTaskResult;
+
+        private void Start()
+        {
+            ExecuteTasks();
+        }
+
+        private async void ExecuteTasks()
+        {
+            if (_tasks == null)
+                return;
+            
+            while (true)
+            {
+                foreach (var task in _tasks)
+                {
+                    _currentTask = task.Execute(this);
+                    _lastTaskResult = await _currentTask;
+                    
+                    if (_lastTaskResult == EnemyTaskResult.Break)
+                        break;
+                }
+                
+                if (_lastTaskResult == EnemyTaskResult.Break)
+                    break;
+            }
+            
+        }
+    }
+}
