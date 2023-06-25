8b9609ca79ef59d7d2bf71f427f3d44ebaba98fd
RandomZiom <dkonik10@gmail.com>
2023-06-25 23:11:25 +0200

    closed game loop

--- Assets/Scripts/GameLoop/GameLoopManager.cs
+++ Assets/Scripts/GameLoop/GameLoopManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Collectables.Abstraction;
+using DG.Tweening;
 using Enemy.Abstraction;
 using GameLoop.Abstraction;
 using Parallax.Abstraction;
@@ -8,6 +9,7 @@ using Player.Abstraction;
 using Player.Shooter;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 namespace GameLoop
@@ -23,10 +25,23 @@ namespace GameLoop
         [SerializeField]
         private List<GameObject> _objectsToEnableOnStart;
         
+        [SerializeField]
+        private List<GameObject> _objectsToEnableOnStop;
+        
+        [SerializeField]
+        private List<GameObject> _objectsToDisableOnStop;
+        
+        [SerializeField]
+        private VolumeProfile _stopGameVolume;
+        
+        [SerializeField]
+        private Volume _globalVolume;
+        
         private IParallaxController _parallaxController;
         private ICollectablesSpawnManager _collectablesSpawnManager;
         private IPlayerMovement _playerMovement;
         private IEnemySpawnerManager _enemySpawnerManager;
+        private IPlayerHealth _playerHealth;
 
         private PlayerShooter _playerShooter;
 
@@ -41,7 +56,10 @@ namespace GameLoop
             _collectablesSpawnManager = ServiceLocator.ServiceLocator.Instance.Get<ICollectablesSpawnManager>();
             _playerMovement = ServiceLocator.ServiceLocator.Instance.Get<IPlayerMovement>();
             _enemySpawnerManager = ServiceLocator.ServiceLocator.Instance.Get<IEnemySpawnerManager>();
+            _playerHealth = ServiceLocator.ServiceLocator.Instance.Get<IPlayerHealth>();
             _playerShooter = _playerSpriteRenderer.GetComponent<PlayerShooter>();
+            
+            _playerHealth.OnPlayerDeath += StopGame;
             
             OnBeforeGameStart();
         }
@@ -49,11 +67,15 @@ namespace GameLoop
         private void OnDestroy()
         {
             ServiceLocator.ServiceLocator.Instance.Deregister<IGameLoopManager>();
+            
+            _playerHealth.OnPlayerDeath -= StopGame;
         }
 
         [Button]
         public void OnBeforeGameStart()
         {
+            Time.timeScale = 1;
+            
             _playerSpriteRenderer.enabled = false;
             _playerShooter.enabled = false;
             
@@ -81,8 +103,25 @@ namespace GameLoop
         [Button]
         public void ResetGame()
         {
+            ServiceLocator.ServiceLocator.Instance.DeregisterAll();
+            DOTween.KillAll();
+            
             var currentScene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(currentScene.buildIndex);
+        }
+
+        [Button]
+        public void StopGame()
+        {
+            _collectablesSpawnManager.SetSpawningState(false);
+            _playerMovement.SetMovementLock(true);
+            _enemySpawnerManager.SetSpawningState(false);
+
+            _objectsToDisableOnStop.ForEach(obj => obj.SetActive(false));
+            _objectsToEnableOnStop.ForEach(obj => obj.SetActive(true));
+            
+            _globalVolume.profile = _stopGameVolume;
+            DOTween.To(() => Time.timeScale, x => Time.timeScale = x, 0, .3f);
         }
     }
 }
